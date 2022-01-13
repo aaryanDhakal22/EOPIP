67474bd08315d7ab04394b13aee8c555a52b23e1
aaryanDhakal22 <aaryan.dhakal22@gmail.com>
2022-01-13 08:19:22 +0545

    Minor adjustments
    some space here and there

--- 1-PrimitiveTypes/multiply.py
+++ 1-PrimitiveTypes/multiply.py
@@ -1,17 +1,20 @@
-def multiply(x,y):
+def multiply(x, y):
     result = 0
-    def add(a,b):
-        while (b!=0):
+
+    def add(a, b):
+        while b != 0:
+            # the carry basically finds out where the set bits in both are common
+            # and later is shifter to represent carry overs
             carry = a & b
             a = a ^ b
             b = carry << 1
         return a
 
-        
     while x:
-        if (x & 1): # if last digit is one
-            result = add(result,y)
-        x, y = x>>1 , y<<1
+        if x & 1:  # if last digit is one
+            result = add(result, y)
+        x, y = x >> 1, y << 1
     return result
 
-print(multiply(15000,3))
+
+print(add(5, 6))
